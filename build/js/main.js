94df91ca14b6930c14976feead3a710e80b87132
andypants152 <andypants152@gmail.com>
2023-10-17 07:13:25 -0400

    in your sights

--- build/js/main.js
+++ build/js/main.js
@@ -1,20 +1,68 @@
 //main.js
 import Screen from './screen.js';
-import Control from './control.js';
 import config from './defaults.js';
 
 class tooDeeWorld {
-    constructor(){
-        this.screen = new Screen(config);
+    constructor() {
+        this.screen = new Screen(config.bgColor);
         this.screen.initialize();
-        this.control = new Control(config, this.screen);
         this.run();
+        this.attachEventListeners();
+
     }
 
-run = () => {
-    this.screen.animate();
-    requestAnimationFrame(this.run)
+    //run = async () => { //i feel like this is going to be right but isn't for now
+    run = () => {
+        //i don't know anymore...
+
+    }
+
+
+
+    attachEventListeners() {
+        window.addEventListener('resize', () => {
+            this.screen.updateCanvasSize();
+        });
+
+        //document.addEventListener('keydown', () => { this.changeBackgroundColor() });
+
+        // // Handle the click event to draw a square
+        // document.addEventListener('click', (event) => {
+        //     const x = event.clientX;
+        //     const y = event.clientY;
+        //     const size = Math.floor(Math.random() * (50 + 1));; // Adjust the size as needed
+        //     const color = 'green'; // Adjust the color as needed
+        //     const square = new Square(x, y, size, this.randomColor());
+        //     this.squares.push(square);
+        // });
+
+        // // Handle the touchstart event to draw a circle
+        // document.addEventListener('touchstart', (event) => {
+        //     const x = event.touches[0].clientX;
+        //     const y = event.touches[0].clientY;
+        //     const color = 'green';
+        //     const size = 50; // Adjust the size as needed
+        //     const circle = new Circle(x, y, size, this.randomColor());
+        //     this.circles.push(circle);
+        // });
+
+        document.addEventListener('keydown', (event) => {
+            switch (event.key) {
+                case ' ':
+                    console.log("toggle?")
+                    this.screen.isAnimating = !this.screen.isAnimating; // Toggle animation state
+                    if (this.screen.isAnimating) {
+                        this.screen.animate(); // Resume animation
+                    }
+                    break;
+                default:
+                    this.screen.changeBackgroundColor();
+                    break;
+
+            }
+        });
+
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => { new tooDeeWorld();});
+document.addEventListener('DOMContentLoaded', () => { new tooDeeWorld(); });
--- build/js/screen.js
+++ build/js/screen.js
@@ -1,14 +1,22 @@
 //screen.js
 export default class Screen {
-    constructor(config) {
+    constructor(color) {
         this.canvas = null;
         this.ctx = null;
-        this.config = config;
+        this.circles = [];
+        this.squares = [];
+        this.color = color;
+        this.frameCount = 0;
+        this.lastTimestamp = Date.now();
+        this.deltaTime = 0;
+        this.isAnimating = true;
+
     }
 
     initialize() {
         this.createCanvas();
         this.updateCanvasSize();
+        this.animate();
     }
 
     createCanvas() {
@@ -19,30 +27,81 @@ export default class Screen {
     }
 
     updateCanvasSize() {
-        console.log("updating");
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
     }
 
     animate() {
-        this.background();
-        //need to add logic for adding elements...
+        if (this.isAnimating) {
 
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.ctx.fillStyle = this.color;
+            this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+
+            this.ctx.lineWidth = 3;
+            this.ctx.strokeStyle = 'red';
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.canvas.width / 2, 0);
+            this.ctx.lineTo(this.canvas.width / 2, this.canvas.height);
+            this.ctx.stroke();
+
+
+            this.ctx.strokeStyle = 'green';
+            this.ctx.beginPath();
+            this.ctx.moveTo(0, this.canvas.height / 2);
+            this.ctx.lineTo(this.canvas.width, this.canvas.height / 2);
+            this.ctx.stroke();
+
+            const text = 'Hello';
+            const textX = this.canvas.width / 2;
+            const textY = this.canvas.height / 2;
+            this.ctx.fillStyle = 'yellow';
+            this.ctx.strokeStyle = 'white';
+
+            this.ctx.font = '80px Helvetica';
+            this.ctx.textAlign = 'center';
+            this.ctx.textBaseline = 'middle';
+            this.ctx.fillText(text, textX,textY);
+            this.ctx.strokeText(text, textX, textY);
+
+
+
+
+
+
+
+            this.logFPS();
+            if (this.isAnimating) {
+                requestAnimationFrame(() => this.animate());
+            }
+        }
     }
 
-    background(){
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.fillStyle = this.config.bgColor; // Use configured background color
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-    changeBackgroundColor() {
-        // Generate a random background color when called
-        //TODO: change to allow a specific color input
-        console.log("huh?")
-        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-        this.config.bgColor = randomColor;
-        this.background(); // Update the background with the new color
+    changeBackgroundColor(color) {
+        if (color) {
+            this.color = color;
+        } else {
+            this.color = this.randomColor();
+        }
     }
 
-    
+    randomColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+
+    logFPS() {
+        const timestamp = Date.now();
+        this.deltaTime += timestamp - this.lastTimestamp;
+        this.lastTimestamp = timestamp;
+        this.frameCount++;
+
+        if (this.deltaTime >= 1000) {
+            console.log(`FPS: ${this.frameCount}`);
+            this.frameCount = 0;
+            this.deltaTime = 0;
+        }
+    }
+
+
 }
