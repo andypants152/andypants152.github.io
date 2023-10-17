6d56cf122e57a84aa961841b95b9730b17dda923
andypants152 <andypants152@gmail.com>
2023-10-17 11:20:45 -0400

    now how can i break this next...

--- build/js/screen.js
+++ build/js/screen.js
@@ -56,7 +56,7 @@ export default class Screen {
 
 
 
-            this.logFPS();
+            // this.logFPS();
             if (this.isAnimating) {
                 requestAnimationFrame(() => this.animate());
             }
